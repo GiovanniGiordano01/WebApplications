6c5979e92a826381665e087aab803af39cfbfc05
Giovanni Giordano <s331574@studenti.polito.it>
2024-05-16 15:47:27 +0200

    prepared LAB08

---
+++ LAB08/react-FilmaLibrary/src/App.jsx
@@ -0,0 +1,57 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavHeader from "./components/NavHeader";
+import Aside from "./components/Aside";
+import Main from "./components/Main";
+import { Film,filmLibrary } from "./FilmLibraryapp";
+import { Container, Row,Col } from 'react-bootstrap';
+import React,{useState} from 'react';
+import './App.css'
+const f = new filmLibrary();
+f.init();
+const films= f.getFilms();
+
+function App() {
+  const [Active,setActive]= useState("All");
+  const [Films, setFilms] = useState(films);
+  const chooseSelected = (s) => {
+    setActive(s);
+  }
+
+  const addFilm = (x) => {
+    setFilms(oldFilms => {
+      const newId = Math.max(...oldFilms.map(movie => movie.id)) + 1;
+      const newFilm = new Film(newId, x.title, x.favorite, x.date,x.rating, 0);
+      f.add(newFilm);
+      return [...oldFilms, newFilm];
+    });
+  }
+  const updateFilm = (x) => {
+    setFilms(oldFilms => {
+      return oldFilms.map((film) => {
+        if(film.id === x.id) {
+          return new Film(x.id, x.title, x.favorite, x.date, x.rating,0);
+        }
+        else
+          return film;
+      });
+    });
+  }
+  return (
+    <>
+    <NavHeader/>
+    <Container fluid className='mt-3'>
+      <Row>
+        <Col md={3}>
+          <Aside choose={chooseSelected} active={Active}/>
+        </Col>
+        <Col md={9}>
+        <Main films={Films} active={Active} addFilm={addFilm} updateFilm={updateFilm}/>
+        </Col> 
+      </Row>
+
+    </Container>
+    </>
+  )
+}
+
+export default App
